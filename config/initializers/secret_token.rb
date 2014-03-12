25fa45466e5ba42ae63a0c66370f27f272b31334
ERNEST SON <sunnee5150@gmail.com>
2014-03-12 18:58:54 -0400

    first upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovRatr::Application.config.secret_key_base = 'f12525ae32ed65f7ca162ed5009f16fa327a0de03b5b43e1658922a92399c9a34470701781227abaaa002c3b028ce5235fba3048d969354c4af65b101103d72d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MovRatr::Application.config.session_store :cookie_store, key: '_mov_ratr_session'
